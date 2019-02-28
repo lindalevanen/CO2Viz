4444e81abd7c17bf48be483e84e50eb087b8a276
Levänen Linda <levanel2@cs.hut.fi>
2019-02-28 23:26:03 +0200

    initialized production build

---
+++ client_co2viz/build/precache-manifest.c4add9b3fa2efbb827dbc8aeb472b3d3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "9419f6d43b3b9a6d55de",
+    "url": "/static/js/main.9419f6d4.chunk.js"
+  },
+  {
+    "revision": "1747fb78326d5ef6e7a3",
+    "url": "/static/js/2.1747fb78.chunk.js"
+  },
+  {
+    "revision": "9419f6d43b3b9a6d55de",
+    "url": "/static/css/main.13b89fb5.chunk.css"
+  },
+  {
+    "revision": "562363322d5d68151d6a9d65487b5680",
+    "url": "/index.html"
+  }
+];
