203329086143f8cb3f6a8c7dcebc8770f34d5110
Levänen Linda <levanel2@cs.hut.fi>
2019-03-01 00:45:37 +0200

    build with npm, modified gitignore

---
+++ client_co2viz/build/precache-manifest.ca0465df3da134d4119067e925ade5f3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "9419f6d43b3b9a6d55de",
+    "url": "/static/js/main.9419f6d4.chunk.js"
+  },
+  {
+    "revision": "b79505da419dbbfa3b93",
+    "url": "/static/js/2.b79505da.chunk.js"
+  },
+  {
+    "revision": "9419f6d43b3b9a6d55de",
+    "url": "/static/css/main.13b89fb5.chunk.css"
+  },
+  {
+    "revision": "8e6e746bbd84c2960d17c0b42f7bb34e",
+    "url": "/index.html"
+  }
+];
